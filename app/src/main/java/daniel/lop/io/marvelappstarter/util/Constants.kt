47149c8635704ca2ed0d42388ef3f6018a4a549c
Matheus Del Nero <matheusdelnerovendas@gmail.com>
2023-03-21 06:08:26 -0300

    Injeção de Dependencia e Service

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,13 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    const val PUBLIC_KEY = "4175da63a56bfb94ea924a591a50c0e1"
+    const val PRIVATE_KEY = "938fdde7049dbff0a97bee8332937787036be2a8"
+
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+
+}
