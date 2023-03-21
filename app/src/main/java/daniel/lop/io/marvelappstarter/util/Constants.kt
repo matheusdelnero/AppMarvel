433007879803d33102d7cc708bfb5cc59b2ece4c
Matheus Del Nero <matheusdelnerovendas@gmail.com>
2023-03-21 17:45:14 -0300

    Busca de Character

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -5,6 +5,8 @@ object Constants {
     const val PUBLIC_KEY = "4175da63a56bfb94ea924a591a50c0e1"
     const val PRIVATE_KEY = "938fdde7049dbff0a97bee8332937787036be2a8"
 
+    const val LAST_SEARCH_QUERY = "last_search_quer"
+    const val DEFAULT_QUERY = ""
 
     const val APIKEY = "apikey"
     const val HASH = "hash"
